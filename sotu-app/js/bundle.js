09eb91c391cba33d05dc5b7b557380f0510b968f
Atomaker123 <9agroup2@npsinr.com>
2026-09-04 13:46:23 +0530

    enter arpith here

--- sotu-app/js/bundle.js
+++ sotu-app/js/bundle.js
@@ -52045,11 +52045,12 @@ const descriptionSplitter_1 = require("../helpers/descriptionSplitter");
 const powToUnit_1 = require("../helpers/powToUnit");
 function getGraphics(visualLocation, textDatum, extraText, units, sizeData) {
     const isMobile = window.innerWidth <= 768;
-    const w = isMobile ? Math.min(320, window.innerWidth - 30) : 450;
+    // On mobile, use generous width and comfortable, large readable typography
+    const w = isMobile ? Math.min(360, window.innerWidth - 20) : 450;
     const h = 500;
     const x = isMobile ? 0 : visualLocation.descriptionX;
     const y = isMobile ? 0 : visualLocation.descriptionY;
-    const margin = isMobile ? 12 : 20;
+    const margin = isMobile ? 16 : 20;
     const baseStyle = {
         fontFamily: 'Roboto',
         align: 'left',
@@ -52057,11 +52058,11 @@ function getGraphics(visualLocation, textDatum, extraText, units, sizeData) {
         wordWrapWidth: w - (margin * 2),
         wordWrap: true
     };
-    const titleStyle = Object.assign({ fontSize: isMobile ? 22 : 40 }, baseStyle);
-    const scaleStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: isMobile ? 16 : 32, fill: 0x333333 });
-    const unitFriendlyStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: isMobile ? 16 : 32, wordWrapWidth: w - (margin * 2), fill: 0x333333, wordWrap: false });
-    const exponentStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: (isMobile ? 16 : scaleStyle.fontSize) - (isMobile ? 5 : 8), fill: 0x333333 });
-    const descriptionStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: isMobile ? 16 : 32 });
+    const titleStyle = Object.assign({ fontSize: isMobile ? 28 : 40, fontWeight: 'bold' }, baseStyle);
+    const scaleStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: isMobile ? 22 : 32, fill: 0x333333 });
+    const unitFriendlyStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: isMobile ? 24 : 32, wordWrapWidth: w - (margin * 2), fill: 0x333333, wordWrap: false, fontWeight: 'bold' });
+    const exponentStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: (isMobile ? 22 : scaleStyle.fontSize) - (isMobile ? 6 : 8), fill: 0x333333 });
+    const descriptionStyle = Object.assign(Object.assign({}, baseStyle), { fontSize: isMobile ? 21 : 32, lineHeight: isMobile ? 26 : 38 });
     const friendly = powToUnit_1.powToUnit(sizeData, units, extraText);
     const splitDescription = descriptionSplitter_1.descriptionSplitter(textDatum.description).replace(/",/g, '');
     const titleText = new PIXI.Text(textDatum.title.replace(/\r?\n|\r/g, ''), titleStyle);
@@ -52076,15 +52077,15 @@ function getGraphics(visualLocation, textDatum, extraText, units, sizeData) {
     titleText.roundPixels = true;
     
     unitFriendlyText.x = x + margin;
-    unitFriendlyText.y = y + titleText.height + (isMobile ? 6 : 25);
+    unitFriendlyText.y = y + titleText.height + (isMobile ? 8 : 25);
     unitFriendlyText.roundPixels = true;
 
     scaleText.x = x + margin;
-    scaleText.y = unitFriendlyText.y + unitFriendlyText.height + (isMobile ? 4 : 5);
+    scaleText.y = unitFriendlyText.y + unitFriendlyText.height + (isMobile ? 6 : 5);
     scaleText.roundPixels = true;
 
     exponentText.x = x + margin + 2.5 + scaleText.width;
-    exponentText.y = scaleText.y - (isMobile ? 5 : 7);
+    exponentText.y = scaleText.y - (isMobile ? 6 : 7);
     exponentText.roundPixels = true;
 
     meterText.x = exponentText.x + exponentText.width + 5;
@@ -52092,24 +52093,23 @@ function getGraphics(visualLocation, textDatum, extraText, units, sizeData) {
     meterText.roundPixels = true;
 
     descriptionText.x = x + margin;
-    descriptionText.y = scaleText.y + scaleText.height + (isMobile ? 8 : 10);
+    descriptionText.y = scaleText.y + scaleText.height + (isMobile ? 12 : 10);
     descriptionText.roundPixels = true;
 
     const descriptionContainer = new PIXI.Container();
     const graphics = new PIXI.Graphics();
     const totalTextHeight = descriptionText.y + descriptionText.height - y + margin;
-    graphics.beginFill(0x000000, .2);
+    graphics.beginFill(0x000000, .25);
     graphics.drawRoundedRect(x + 5, y + 5, w, totalTextHeight, 15);
     graphics.endFill();
-    graphics.lineStyle(2, 0xaaaaaa, 1);
-    graphics.beginFill(0xFFFFFF, 1);
+    graphics.lineStyle(2, 0x999999, 1);
+    graphics.beginFill(0xFFFFFF, 0.98);
     let widthToUse = w;
     if (unitFriendlyText.width + (margin * 2) >= widthToUse) {
         widthToUse = unitFriendlyText.width + (margin * 2);
     }
     graphics.drawRoundedRect(x, y, widthToUse, totalTextHeight, 15);
     graphics.endFill();
-    graphics.alpha = .95;
     descriptionContainer.x -= widthToUse / 2;
     descriptionContainer.y -= isMobile ? (totalTextHeight / 2) : (h / 2);
     descriptionContainer.addChild(graphics);
